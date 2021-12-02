ae16c9c62ca820aec131e5de610e6f704d53278f
Nongus Studios <jandrewgadd@gmail.com>
2021-12-02 17:25:47 +1300

    dwm, dwmblocks, and st

---
+++ dwmblocks/blocks.def.h
@@ -0,0 +1,15 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	//{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+    {"  ",  "/home/joel/.config/dwmblocks/scripts/cpu-perc.sh",     4,              0},
+    {" ",   "free -m | awk 'NR==2{printf \"%.0f%%\", $3*100/$2 }'", 5,              0},
+    {" ",   "pacman -Q | wc -l",                                    5,              0}, 
+    {"",     "sb-mediaplayer",                                       1,              1},
+    {"",     "sb-volume",                                            1,              2},
+	{"",     "date '+%I:%M %a, %d %b '",					         20,   	         0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
