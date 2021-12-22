7805b38c1555aa65f5a8419e44b84dd211358386
Nongus Studios <jandrewgadd@gmail.com>
2021-12-23 06:51:40 +1300

    Added shortcut for brave browser

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -65,12 +65,14 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_txt, "-sb", col_acc2, "-sf", col_bg, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st",    NULL };
+static const char *bravecmd[] = { "brave", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd  } },
+    { MODKEY|ShiftMask,             XK_b,      spawn,          {.v = bravecmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
