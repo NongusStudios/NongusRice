00f3ac2f85337f7bba65c8c99a75c3ad0ec4ab61
Nongus Studios <jandrewgadd@gmail.com>
2022-03-08 16:22:55 +1300

    oop

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 cd dwm
 sudo make clean install
-cp config.h config.def.h
 cd ../dwmblocks
 sudo make clean install
-cp blocks.h blocks.def.h
 cd ../st
 sudo make clean install
-cp config.h config.def.h
 cd ..
 
 mkdir -p $HOME/.config/dunst
